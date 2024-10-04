289c7c376c74c90787150a74650f25742e18bbb7
Danielhhgfj <henrique.matheus.souza@escola.pr.gov.br>
2024-10-04 08:53:59 -0300

    hghmfgfmh

--- main.js
+++ main.js
@@ -1,5 +1,9 @@
  let estados=['AC','AL','AP','AM','BA','CE','ES','GO','MA','MT','MS','MG','PA','PB','PR','PE','PI','RJ','RN','RS','RO','RR','SC','SP','SE','TO','DF'];
-
+ let i=0;
+ while(i<10){
+ console.log(i);
+ i=i+1;
+}
 
 
 
