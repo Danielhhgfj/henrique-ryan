85b6e074dae956afb696cc1a894e3a287b9252c2
Danielhhgfj <henrique.matheus.souza@escola.pr.gov.br>
2024-10-04 09:03:22 -0300

    \dffsf\df

--- main.js
+++ main.js
@@ -1,9 +1,9 @@
  let estados=['AC','AL','AP','AM','BA','CE','ES','GO','MA','MT','MS','MG','PA','PB','PR','PE','PI','RJ','RN','RS','RO','RR','SC','SP','SE','TO','DF'];
  let i=0;
- while(i<10){
+ while(i<27){
  console.log(i);
  console.log(estados[i]);
- i++27;
+ i++;
 }
 
 
